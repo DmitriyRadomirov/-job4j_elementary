3b0cf893c0110ccf4ee6646851c8aed8a948961e
Dmitriy Radomirov <dibradim@gmail.com>
2022-02-14 16:04:08 +0300

    4.2. Заголовок в файле java

--- src/main/java/ru/job4j/Studentinfo.java
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
-public class Studentinfo{
-    public static void main(String[] args){
+public class Studentinfo {
+    public static void main(String[] args) {
         System.out.println("Dmitriy Radomirov");
         System.out.println("27.05.1976");
     }
