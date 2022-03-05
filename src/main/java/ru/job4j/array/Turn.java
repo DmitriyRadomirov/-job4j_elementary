59899912dac0a9ce190bb3a7dbecd2680dfe2f62
Dmitriy Radomirov <dibradim@gmail.com>
2022-03-05 19:06:10 +0300

    6.3. Массив заполнен true или false [#53857]

--- src/main/java/ru/job4j/array/Turn.java
+++ src/main/java/ru/job4j/array/Turn.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class Turn {
     public static int[] back(int[] array) {
-        for (int index = 0; index < 1; index++) {
+         for (int index = 0; index < 1; index++) {
             int temp = array[index];
             array[index] = array[array.length - 1];
             array[array.length - 1] = temp;
