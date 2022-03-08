a8f9dd6849dc1edd6a24e364624fa89c2f094ac6
Dmitriy Radomirov <dibradim@gmail.com>
2022-03-08 11:16:09 +0300

    6.5. Поиск индекса в диапазоне. [#156318 #272565]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -4,12 +4,11 @@ public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
           for (int i = 0; i < post.length - 1; i++) {
-            for (int j = 0; j < post.length - 1; j++) {
-                if (word[word.length - 1 - i] != post[post.length - 1 - j]) {
+                if (word[word.length - 1 - i] != post[post.length - 1 - i]) {
                     result = false;
+                    break;
                 }
             }
-        }
     return result;
     }
 }
