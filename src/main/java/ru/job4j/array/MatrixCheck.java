9d6989f76644eb72844febaf40f0afa0349f9c71
Dmitriy Radomirov <dibradim@gmail.com>
2022-03-09 22:03:40 +0300

    6.7.4. Выигрышные комбинации в сокобан [#53859 #273078]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -34,11 +34,12 @@ public class MatrixCheck {
     public static boolean isWin(char[][] board) {
         boolean result = false;
         for (int i = 0; i < board.length; i++) {
-            if (monoHorizontal(board, i) || monoVertical(board, i)) {
-                result = true;
-                break;
+            extractDiagonal(board);
+             if (monoHorizontal(board, i) || monoVertical(board, i)) {
+                    result = true;
+                    break;
+                }
             }
-        }
         return result;
     }
 }
