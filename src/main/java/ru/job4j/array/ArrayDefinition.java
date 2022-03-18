fe11812081a8ffe90777e299498fe15698b4d16e
Dmitriy Radomirov <dibradim@gmail.com>
2022-03-18 17:14:06 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,12 +3,10 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
                String[] surnames = new String[4];
-
                 surnames[0] = "Spartak";
                 surnames[1] = "Ben";
                 surnames[2] = "Iggi";
                 surnames[3] = "Harry";
-
                 System.out.println(surnames[0]);
                 System.out.println(surnames[1]);
                 System.out.println(surnames[2]);
--- src/main/java/ru/job4j/array/EqLast.java
+++ src/main/java/ru/job4j/array/EqLast.java
@@ -5,7 +5,6 @@ public class EqLast {
          if (left[left.length - 1] == right[right.length - 1]) {
                 return true;
             }
-
         return false;
     }
 }
